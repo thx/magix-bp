154b5c4dfcbbc2f0945f717a55f301b2ec1c7fd3
Chen Yicai <yicai.cyj@taobao.com>
2013-11-19 19:38:30 +0800

    Finished step 1, basic layout of magix bp app

---
+++ app/common/views/header.js
@@ -0,0 +1,9 @@
+KISSY.add("app/common/views/header", function(S, View) {
+    return View.extend({
+        render : function() {
+            this.setViewHTML(this.template);
+        }
+    });
+}, {
+    requires : ['magix/view']
+});
--- app/ini.js
+++ app/ini.js
@@ -2,7 +2,7 @@ KISSY.add('app/ini', function(S) {
     return {
         defaultView: 'app/common/views/default',
         routes: function(pathname) {
-            return this.defaultView;
+            return pathname.indexOf('app/') === 0 ? pathname : this.defaultView
         }
     };
 });
